06c51045d6bc0878480040637cb08e455f13f307
krushna <krushnasakhare965@gmail.com>
2024-10-16 15:04:01 +0530

    Signin file url edited

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -10,8 +10,7 @@ import { useState } from "react"
 import AuthLoader from "@/loaders/AuthLoader"
 import { useNavigate } from "react-router-dom"
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 
 export default function SignIn({ setShowRegister, setShowSignin }) {
